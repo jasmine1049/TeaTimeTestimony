09ec0c805615059bd5f5e275ed0264adb9f68702
riverchill <48643422+riverchill@users.noreply.github.com>
2023-01-31 23:40:23 -0800

    Simple Player Movement
    Player and Camera Movement

---
+++ Assets/Player Assets/Scripts/CameraConroller.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraConroller : MonoBehaviour
+{
+    public Transform player;
+    private Vector3 offset;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + offset;
+    }
+}
