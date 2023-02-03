2cec14edfea992b67200d11f6505a0d3c25329bb
braydenrudisill <braydenrudisill@gmail.com>
2023-02-03 09:57:18 -0800

    Fix Typo

--- Assets/Player Assets/Scripts/CameraController.cs
+++ Assets/Player Assets/Scripts/CameraController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraConroller : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
     public Transform player;
     private Vector3 offset;
